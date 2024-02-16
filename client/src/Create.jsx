b36391ee13af00d6eed7a2f2b6915ebe76c291b0
aagneye-syam <saagneye2003@gmail.com>
2024-02-16 10:57:46 +0530

    Hook and Create component added

---
+++ client/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Create() {
+  return (
+    <div className="create_form">
+      <input type="text" name="" id="" />
+      <button type="button">Add</button>
+    </div>
+  );
+}
+
+export default Create;
---
+++ client/src/Hook.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Create from './Create'
+
+function Hook() {
+  return (
+    <div>
+        <h1>ToDo List</h1>
+        <Create />
+        
+    </div>
+  )
+}
+
+export default Hook
