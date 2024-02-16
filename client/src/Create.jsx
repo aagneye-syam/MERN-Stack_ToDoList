a897f9c8f8734d031d71874fdc2898103e4377bf
aagneye-syam <saagneye2003@gmail.com>
2024-02-16 23:38:04 +0530

    api establishment

--- client/src/Create.jsx
+++ client/src/Create.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 function Create() {
+  const [task, setTask] = useState();
+  const handleAdd = () => {
+        axios.post('http://localhost:3001/add',{task:task}).then(result => console.log(result)).catch(err => console.log(err))
+  };
   return (
     <div className="create_form">
-      <input type="text" name="" id="" />
-      <button type="button">Add</button>
+      <input type="text" name="" id="" placeholder="Enter task" onAbort={() => setTask(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
     </div>
   );
 }
