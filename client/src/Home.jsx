512501f96ade57981bdae61c4c275454ceb4bc71
aagneye-syam <saagneye2003@gmail.com>
2024-02-17 12:01:19 +0530

    trash bin added and styled

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Create from "./Create";
-import axios from 'axios'
-import {BsCircleFill ,BsFillTrashFill} from "react-icons/bs"
+import axios from "axios";
+import { BsCircleFill, BsFillTrashFill } from "react-icons/bs";
 
 function Home() {
   const [toDos, setToDos] = useState([]);
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/get')
-      .then(result => setToDos(result.data))
-      .catch(err => console.log(err));
+      .get("http://localhost:3001/get")
+      .then((result) => setToDos(result.data))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
@@ -18,15 +18,20 @@ function Home() {
       <h1>ToDo List</h1>
       <Create />
       {toDos.length === 0 ? (
-        <div className='task' >
+        <div className="task">
           <h2>No record</h2>
         </div>
-      ) : ( 
+      ) : (
         toDos.map((toDo, index) => (
-          <div className='task' key={index}>
+          <div className="task" key={index}>
             <div className="checkbox">
-            <BsCircleFill className='icon' />
-            <p>{toDo.task}</p>
+              <BsCircleFill className="icon" />
+              <p>{toDo.task}</p>
+            </div>
+            <div>
+              <span>
+                <BsFillTrashFill className="icon" />
+              </span>
             </div>
           </div>
         ))
