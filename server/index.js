6971f9ef48f5e2c582b6745288196256f223cdc4
aagneye-syam <saagneye2003@gmail.com>
2024-02-17 08:38:22 +0530

    task added to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const TodoModel = require('./models/ToDo')
+const TodoModel = require("./models/ToDo");
 
 const app = express();
 app.use(cors());
@@ -9,7 +9,14 @@ app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/test");
 
-
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({
+    task: task,
+  })
+    .then((result) => req.json(result))
+    .catch((err) => req.json(err));
+});
 
 PORT = 3001;
 
